e56c599b6e82a94e138253671921c470de995346
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-06 02:29:05 +0600

    Frist messages

---
+++ script.js
@@ -0,0 +1,16 @@
+function onePlusAvg(x, y){
+    console.log("Done")
+    return 1 + (x + y)/2
+}
+const sum = (p ,q)=>{
+    return p + q
+}
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("one plus Average of a and b is ", onePlusAvg(a, b))
+console.log("one plus Average of a and b is ", onePlusAvg(b, c))
+console.log("one plus Average of a and b is ", onePlusAvg(c, a))
+console.log(sum(9, 7))
+
